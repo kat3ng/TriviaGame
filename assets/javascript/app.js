ec5abc59207f67d92ffea9817ab1a1978948990e
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-09-30 14:59:33 -0500

    html built out and linked up to style and js sheets

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+document.readyState(function{
+
+    // create a startGame function
+    function startGame() {
+
+    }
+    // starts timer on click.
+    $("#start").on("click", function () {
+        //  Set the button alert's timeout to run three seconds after the function's called.
+        delayButtonAlert = setTimeout(function () {
+            alert("Alert #2: Called 3 seconds after the start button is clicked.");
+        }, 120000);
+    });
+
+
+    // changes screen to reveal questions and choices belows below question (can be true or false OR multiple choice)
+    // limit responses to one per question
+
+    // timer ends 
+    // stops activity
+    // calculates score
+    //changes screen to 
+
+    // finish function
+    // stops activity
+    // stops timer
+    // calculates score
+
+    // restart function?
+}
