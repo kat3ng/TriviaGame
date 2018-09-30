df7be4825750548663d1253d8ebdeafb49e3fc2d
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-09-30 16:12:11 -0500

    questioning begins

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,29 @@
-document.readyState(function{
-
-    // create a startGame function
-    function startGame() {
-
-    }
-    // starts timer on click.
-    $("#start").on("click", function () {
-        //  Set the button alert's timeout to run three seconds after the function's called.
-        delayButtonAlert = setTimeout(function () {
-            alert("Alert #2: Called 3 seconds after the start button is clicked.");
-        }, 120000);
-    });
+// Create a function that creates the start button and initial screen
+$(document).ready(function () {
 
 
-    // changes screen to reveal questions and choices belows below question (can be true or false OR multiple choice)
-    // limit responses to one per question
+    // Create trivia questions. 
+    var questions = [{
+        question: "How many 'inner' senshi exist within the Sailor Moon series?",
+        answer: ["5", "4", "8", "3"],
+        qname: "inner-senshi",
+        correct: "5",
+        //! Mercury, Mars, Venus and Jupiter, alongside Sailor Moon make up the inner senshi in the Sailor Moon series."
+        divClass: ".innersenshi"
+    },
+    {
+        question: "Princess Serenity, daughter of Queen Serenity, was the princess of what kingdom?",
+        answer: ["The Moon Kingdom", "Kingdom Heart", "Silver Millenium", "Europa"],
+        qname: "moon-kingdom",
+        correct: "The Moon Kingdom",
+        divClass: ".moonkingdom"
+    },
+        question: "Prince Endymion and Princess Serenity of the future have a time-traveling daughter who goes back in time to seek help from Serena and her friends in the present, what is her name?",
+        answer: ["Hillary", "Chibi Chibi", "Serene", "Chibi Usa"],
+        qname: "moon-kingdom",
+        correct: "The Moon Kingdom",
+        divClass: ".moonkingdom"
+    },
 
-    // timer ends 
-    // stops activity
-    // calculates score
-    //changes screen to 
-
-    // finish function
-    // stops activity
-    // stops timer
-    // calculates score
-
-    // restart function?
-}
+    ] // end questions object
+};
